c292fd446baf4edf8bb936e0dacfd58d1d37681c
Narayan Acharya <narayan.acharya@sony.com>
2020-07-08 11:34:54 -0400

    Fixes and changes

--- NLP/nlp-pipeline/app.py
+++ NLP/nlp-pipeline/app.py
@@ -8,11 +8,7 @@ from slack.errors import SlackApiError
 
 from state import State
 
-# TODO: Get rid of this, should be set by an external script instead of code
-os.environ['SLACK_API_TOKEN'] = 'xoxb-1210735252167-1224343562405-iPaYFMC9P4HouHAUpV30yib3'
-
 SLACK_API_TOKEN = os.environ['SLACK_API_TOKEN']
-
 TEAM_1, TEAM_2 = '#teamBlue', '#teamRed'
 
 app = Flask(__name__)
@@ -28,21 +24,35 @@ def send_message(message):
         print(e.response)
 
 
+def load_dummy_messages():
+    print('Loading dummy messages...')
+    with open('dummy.txt', encoding="utf8") as f:
+        for line in f.readlines():
+            if TEAM_1 in line:
+                state.add_message(TEAM_1, line)
+            elif TEAM_2 in line:
+                state.add_message(TEAM_2, line)
+    print('Done loading dummy messages...')
+
+
 def loop():
     print('In the loop! Will send summaries if there exists...')
     summary_1, summary_2 = state.get_summary(TEAM_1), state.get_summary(TEAM_2)
+    sentiment_1, sentiment_2 = state.get_sentiment(TEAM_1), state.get_sentiment(TEAM_2)
     if summary_1:
-        send_message(TEAM_1 + ' : ' + summary_1)
+        send_message(TEAM_1 + '(' + str(sentiment_1) + ')' + ' : ' + summary_1)
 
     if summary_2:
-        send_message(TEAM_2 + ' : ' + summary_2)
+        send_message(TEAM_2 + '(' + str(sentiment_2) + ')' + ' : ' + summary_2)
 
     threading.Timer(60, loop).start()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def slack_incoming():
+    # This is required for setting up the callback URL when new events occur
     # return json.loads(request.data)['challenge']
+
     data = json.loads(request.data)
     print('Incoming data: ', data)
 
@@ -50,10 +60,8 @@ def slack_incoming():
         content = data['event']['text']
         if TEAM_1 in content:
             state.add_message(TEAM_1, content)
-            send_message(state.get_sentiment(TEAM_1))
         elif TEAM_2 in content:
             state.add_message(TEAM_2, content)
-            send_message(state.get_sentiment(TEAM_2))
 
     return Response(), 200
 
@@ -78,4 +86,5 @@ def get_metrics():
     return {TEAM_1: state.get_metrics(TEAM_1), TEAM_2: state.get_metrics(TEAM_2)}
 
 
+load_dummy_messages()
 loop()
--- NLP/nlp-pipeline/state.py
+++ NLP/nlp-pipeline/state.py
@@ -3,10 +3,10 @@ from transformers import pipeline
 PIPELINE_SENTIMENT = 'sentiment-analysis'
 PIPELINE_SUMMARIZE = 'summarization'
 
-SUMMARIZER_MODEL = 't5-base'
-SUMMARIZER_TOKENIZER = 't5-base'
+# SUMMARIZER_MODEL = 't5-base'
+# SUMMARIZER_TOKENIZER = 't5-base'
 SUMMARIZER_MIN_LEN = 10
-SUMMARIZER_MAX_LEN = 100
+SUMMARIZER_MAX_LEN = 50
 
 
 class State:
@@ -17,16 +17,17 @@ class State:
         self.cache[team_b] = []
 
         self.sentiment_analyzer = pipeline(PIPELINE_SENTIMENT)
-        self.summarizer = pipeline(PIPELINE_SUMMARIZE, model=SUMMARIZER_MODEL, tokenizer=SUMMARIZER_TOKENIZER)
+        self.summarizer = pipeline(PIPELINE_SUMMARIZE)
 
     def add_message(self, team_name, message):
         message = message.replace(team_name, '')
         sentiment = self.sentiment_analyzer(message)[0]
         sentiment['message'] = message
+        print(sentiment)
         self.cache.get(team_name).append(sentiment)
 
     def get_content(self, team_name):
-        return '.'.join([item['message'] for item in self.cache.get(team_name)]).strip()
+        return ' '.join([item['message'] for item in self.cache.get(team_name)]).strip()
 
     def get_summary(self, team_name):
         content = self.get_content(team_name)
@@ -52,12 +53,18 @@ class State:
                 'positive_avg': 0 if len(positive_messages) == 0 else sum(positive_messages) / len(positive_messages),
                 'negative_avg': 0 if len(negative_messages) == 0 else sum(negative_messages) / len(negative_messages)}
 
-    def get_sentiment(self, team_name):
-        content = self.get_content(team_name)
-        if content == '' or content is None:
-            return None
+    def get_sentiment_for_message(self, message):
+        return self.sentiment_analyzer(message)[0]
 
-        return self.sentiment_analyzer(content)[0]
+    def get_sentiment(self, team_name):
+        scores = []
+        for item in self.cache.get(team_name):
+            if item['label'] == 'POSITIVE':
+                scores.append(item['score'])
+            else:
+                scores.append(-item['score'])
+
+        return 0 if len(scores) == 0 else sum(scores) / len(scores)
 
 
 if __name__ == '__main__':
@@ -66,10 +73,14 @@ if __name__ == '__main__':
     state = State(TEAM_BLUE, TEAM_RED)
     state.add_message(TEAM_BLUE, 'This Liverpool team doesnt look like their typical selves! #teamBlue')
     state.add_message(TEAM_BLUE, 'If you dont make liverpool win every game Im going to eat you. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Liverpool were caught on the break yet again. Unlike City, Villa didnt have the quality to take advantage of it. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Liverpool’s biggest weakness is the counter-attack defense. They are getting counterattacked like crazy, just watch Thursday’s game with Man City and even today. Needs to get fixed asap. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Curtis Jones had a blast of a performance. No wonder why Liverpool gave him an contract extention. The future is bright for him. #teamBlue')
-    state.add_message(TEAM_BLUE, 'You can tell by our performance against City that the boys mentality has changed a bit. Hopefully this W lights a fire under them. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Liverpool were caught on the break yet again. Unlike City, Villa didnt have the quality to take advantage of it. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Liverpool’s biggest weakness is the counter-attack defense. They are getting counterattacked like crazy, just watch Thursday’s game with Man City and even today. Needs to get fixed asap. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Curtis Jones had a blast of a performance. No wonder why Liverpool gave him an contract extention. The future is bright for him. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'You can tell by our performance against City that the boys mentality has changed a bit. Hopefully this W lights a fire under them. #teamBlue')
     state.add_message(TEAM_BLUE, 'My mans Pepe Reina still playing to this day! Anfield legend. #teamBlue')
     state.add_message(TEAM_BLUE, 'Aston Villa had so many chances, only if they had a solid finisher. #teamBlue')
     state.add_message(TEAM_BLUE, 'Allison is surely the best goalkeeper in the league at the moment. #teamBlue')
@@ -77,20 +88,26 @@ if __name__ == '__main__':
     state.add_message(TEAM_BLUE, 'Missed Reina. Hope he does well. #teamBlue')
     state.add_message(TEAM_BLUE, 'Mo salah he’s a mean man for Liverpool. #teamBlue')
     state.add_message(TEAM_BLUE, 'Bruh Alisson picking that pass out at 7:22 is crazy. #teamBlue')
-    state.add_message(TEAM_BLUE, 'IM LITERALLY SO HAPPY LIVERPOOL WON TODAY WHILE MAN CITY LOST WHERES YOUR CONFIDENCE NOW MAN CITY?! #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'IM LITERALLY SO HAPPY LIVERPOOL WON TODAY WHILE MAN CITY LOST WHERES YOUR CONFIDENCE NOW MAN CITY?! #teamBlue')
     state.add_message(TEAM_BLUE, 'I think we could get Mings on the cheap, you know? #teamBlue')
     state.add_message(TEAM_BLUE, 'Salah incredible man! What a assist. #teamBlue')
     state.add_message(TEAM_BLUE, 'Another win, another points. Go Liverpool YNWA. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Liverpool been getting exposed at the back on counters, full backs are far too high and its just VVD and Gomez to have to rush back. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Liverpool been getting exposed at the back on counters, full backs are far too high and its just VVD and Gomez to have to rush back. #teamBlue')
     state.add_message(TEAM_BLUE, 'Virgil been in bad form recently. Looks like hes not focused. #teamBlue')
     state.add_message(TEAM_BLUE, 'They should of give penalty. #teamBlue')
     state.add_message(TEAM_BLUE, 'Ox has his wheels back on.  Well done. #teamBlue')
     state.add_message(TEAM_BLUE, 'I guess the hangover finally wore off. #teamBlue')
-    state.add_message(TEAM_BLUE, '11:52 that is terrible defensive position by Trent, he needs to make sure he’s alert on his defensive duties when Gomez switches. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Klopp and peps faces after Jones scored, it probably means theyre gonna start him next lol. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Mo is dancing all over Villa. Someone please understand how it’s not a foul. If kane does it it’s a pen all day. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      '11:52 that is terrible defensive position by Trent, he needs to make sure he’s alert on his defensive duties when Gomez switches. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Klopp and peps faces after Jones scored, it probably means theyre gonna start him next lol. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Mo is dancing all over Villa. Someone please understand how it’s not a foul. If kane does it it’s a pen all day. #teamBlue')
     state.add_message(TEAM_BLUE, 'Man City is losing 1-0 to Southampton lmao. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Grealish seems like Aston villas only good player. But he couldn’t finish today. They deserve to be relegated. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Grealish seems like Aston villas only good player. But he couldn’t finish today. They deserve to be relegated. #teamBlue')
     state.add_message(TEAM_BLUE, 'how is that not a penalty! Wtf?!?. #teamBlue')
     state.add_message(TEAM_BLUE, 'Those subs saved us. #teamBlue')
     state.add_message(TEAM_BLUE, 'AV played a pretty good game against the champs. - Geaux Reds. #teamBlue')
@@ -100,8 +117,10 @@ if __name__ == '__main__':
     state.add_message(TEAM_BLUE, 'If you dont make liverpool win every game Im going to eat you. #teamBlue')
     state.add_message(TEAM_BLUE, 'alisson had to put a bookmark on his book that he brought. #teamBlue')
     state.add_message(TEAM_BLUE, 'I want to see Minamono play more, who agrees. #teamBlue')
-    state.add_message(TEAM_BLUE, 'who else have difficulty differentiate the two team due to their colors?!!! FML. #teamBlue')
-    state.add_message(TEAM_BLUE, 'funny how Man City thought they beat Liverpool because they were the better team not because Liverpool dgaf anymore. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'who else have difficulty differentiate the two team due to their colors?!!! FML. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'funny how Man City thought they beat Liverpool because they were the better team not because Liverpool dgaf anymore. #teamBlue')
     state.add_message(TEAM_BLUE, 'These were the worst highlights I’ve ever seen. #teamBlue')
     state.add_message(TEAM_BLUE, '6:50 camera man thinkin bout somethin. #teamBlue')
     state.add_message(TEAM_BLUE, 'Naby keita had a game today nice assist. #teamBlue')
@@ -109,11 +128,14 @@ if __name__ == '__main__':
     state.add_message(TEAM_BLUE, 'Mane is simply the best! #teamBlue')
     state.add_message(TEAM_BLUE, 'there were so many counter attacks in this game. #teamBlue')
     state.add_message(TEAM_BLUE, 'This Villa team will give United problems on Thursday. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Not sure if Naby is gonna make it, gave up the ball often leading to a counter. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Not sure if Naby is gonna make it, gave up the ball often leading to a counter. #teamBlue')
     state.add_message(TEAM_BLUE, 'Don’t like him paired up with Ox, different team with Gini and Hendo. #teamBlue')
     state.add_message(TEAM_BLUE, 'THE WORLD IN GOD JUDGMENTDAY. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Dont worry, Nottingham Forest will sneak into the Premier League, and represent the British Midland region property. #teamBlue')
-    state.add_message(TEAM_BLUE, 'Goodbye Aston Villa! Birmingham City will be waiting for you in the Champions League! LOL!!! #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Dont worry, Nottingham Forest will sneak into the Premier League, and represent the British Midland region property. #teamBlue')
+    state.add_message(TEAM_BLUE,
+                      'Goodbye Aston Villa! Birmingham City will be waiting for you in the Champions League! LOL!!! #teamBlue')
 
     state.add_message(TEAM_RED, 'Liverpool come on....give the little guys in the Prem a chance. #teamRed')
     state.add_message(TEAM_RED, 'Gini has earned a new contract. #teamRed')
@@ -137,7 +159,8 @@ if __name__ == '__main__':
     state.add_message(TEAM_RED, 'Aston villa had everything right besides marking. #teamRed')
     state.add_message(TEAM_RED, 'SADIO MANE for PL player of the year. #teamRed')
     state.add_message(TEAM_RED, 'Why do I hear a crowd of no ones there. #teamRed')
-    state.add_message(TEAM_RED, 'liverpool dont look like epl champions. Its not often that I am impressed by their performance. City is a better team. #teamRed')
+    state.add_message(TEAM_RED,
+                      'liverpool dont look like epl champions. Its not often that I am impressed by their performance. City is a better team. #teamRed')
     state.add_message(TEAM_RED, 'What happened minamino ??? #teamRed')
     state.add_message(TEAM_RED, 'Why is he not giving Minamino minutes? #teamRed')
     state.add_message(TEAM_RED, 'Awesome game champs. #teamRed')
@@ -147,24 +170,33 @@ if __name__ == '__main__':
     state.add_message(TEAM_RED, 'if grealish gets injured bye bye villa. #teamRed')
     state.add_message(TEAM_RED, 'Hello big fan. #teamRed')
     state.add_message(TEAM_RED, 'Why could I hear a crowd? #teamRed')
-    state.add_message(TEAM_RED, 'How is it that PL games are continuing when Liverpool was named the champions? Someone explain please! #teamRed')
+    state.add_message(TEAM_RED,
+                      'How is it that PL games are continuing when Liverpool was named the champions? Someone explain please! #teamRed')
     state.add_message(TEAM_RED, 'Sadio MAIN, the main man for Liverpool! #teamRed')
     state.add_message(TEAM_RED, 'Without the fans, the game is more like a training session. #teamRed')
-    state.add_message(TEAM_RED, 'stagnant in the first half but looked a lot better in the second half especially after Hendo came on. #teamRed')
+    state.add_message(TEAM_RED,
+                      'stagnant in the first half but looked a lot better in the second half especially after Hendo came on. #teamRed')
     state.add_message(TEAM_RED, 'Nice to have the best GK in the world also, he made some key saves YNWA. #teamRed')
     state.add_message(TEAM_RED, 'My only question is Between SALAH and JONES who got the curler HAIR. #teamRed')
     state.add_message(TEAM_RED, 'Allison is the best in the world. #teamRed')
     state.add_message(TEAM_RED, 'This was some sloppy soccer. #teamRed')
-    state.add_message(TEAM_RED, 'ove how happy Hendo is for Curtis on the goal, even though he couldve hit that himself. What a captain. #teamRed')
+    state.add_message(TEAM_RED,
+                      'ove how happy Hendo is for Curtis on the goal, even though he couldve hit that himself. What a captain. #teamRed')
     state.add_message(TEAM_RED, 'Liverpool is struggling here. A lot of bad passes. #teamRed')
-    state.add_message(TEAM_RED, 'You can just tell how reluctant Salah is to use his right foot, even just to pass. He’s incredible on his left but if it’s all he uses he’s a limited player honestly. #teamRed')
+    state.add_message(TEAM_RED,
+                      'You can just tell how reluctant Salah is to use his right foot, even just to pass. He’s incredible on his left but if it’s all he uses he’s a limited player honestly. #teamRed')
     state.add_message(TEAM_RED, 'Turn off virtual fans. #teamRed')
     state.add_message(TEAM_RED, 'Is mane Muslim? #teamRed')
     state.add_message(TEAM_RED, 'Liverpool is just a tryhard. #teamRed')
-    state.add_message(TEAM_RED, 'NBC WILL PRAISE A WHITE CAPTAIN “Henderson” giving him achievement of black player like MANE, GIGI, Alexander, VIRGIL..while they ignored “Kompany” during Mancity days just praising aguero then Kevin De Bruyen. #teamRed')
+    state.add_message(TEAM_RED,
+                      'NBC WILL PRAISE A WHITE CAPTAIN “Henderson” giving him achievement of black player like MANE, GIGI, Alexander, VIRGIL..while they ignored “Kompany” during Mancity days just praising aguero then Kevin De Bruyen. #teamRed')
     state.add_message(TEAM_RED, 'A Win For Liverpool. #teamRed')
-    state.add_message(TEAM_RED, 'How does a religiously observant person like Mo Salah square his faith with kneeling before games? #teamRed')
+    state.add_message(TEAM_RED,
+                      'How does a religiously observant person like Mo Salah square his faith with kneeling before games? #teamRed')
     state.add_message(TEAM_RED, 'this fake fan and stadium noise is terrible. #teamRed')
 
-    print(state.get_summary(TEAM_BLUE))
-    print(state.get_summary(TEAM_RED))
+    print('Blue Team Summary: ', state.get_summary(TEAM_BLUE))
+    print('Red Team Summary: ', state.get_summary(TEAM_RED))
+
+    print('Blue Team Avg Sentiment Intensity: ', state.get_sentiment(TEAM_BLUE))
+    print('Red Team Avg Sentiment Intensity: ', state.get_sentiment(TEAM_RED))
